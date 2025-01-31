500972e28ac645770a8911ac287e6466d7b7eea4
Jakob <sokolov.yakov@inbox.ru>
2025-01-31 23:20:51 +0500

    10.10.1

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post, Category
+from .forms import PostCreateForm
 
 
 class PostListView(ListView):
@@ -43,3 +44,20 @@ class PostFormCategory(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = f'Записи из категории: {self.category.title}'
         return context
+
+class PostCreateView(CreateView):
+    '''
+    Представление: создание материалов на сайте
+    '''
+    model = Post
+    template_name = 'blog/post_create.html'
+    form_class = PostCreateForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи на сайт'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
